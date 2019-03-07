69f5587ceb985ebf5d1b77ba9aa29feb561171e8
Aman Albogast <albogast.aman@gmail.com>
2019-03-08 00:25:38 +0300

    Added config folder and added knex file

--- src/models/participants.js
+++ src/models/participants.js
@@ -1,20 +1,23 @@
 'use strict';
 var Mockgen = require('./mockgen.js');
-const conn=require("../../knexfile").development;
+const conn=require("../../config/knexfile").development;
 const knex = require('knex')(conn);
 /**
  * Operations on /participants
  */
 
-exports.getParticipant=(bankCode)=>{
-       return knex.select('p.participantName as fspId','c.name as currency', knex.raw('? as ??', ['null', 'partySubIdOrType'])).from('currencyParticipant as cp').innerJoin('participant as p', 'cp.participantId', 'p.participantId')
+exports.getParticipant = async (bankCode)=>{
+       var participant=null;
+       await knex.select('p.participantName as fspId','c.name as currency', knex.raw('? as ??', ['null', 'partySubIdOrType'])).from('currencyParticipant as cp').innerJoin('participant as p', 'cp.participantId', 'p.participantId')
             .innerJoin('currency as c', 'c.currencyId', 'cp.currencyId').where({
                 'p.participantName':bankCode,
                 'p.isActive': 1
             }).then((fsp)=>{
-                return fsp;
+                participant=fsp;
             }).catch((err)=>{
                 console.log(err); throw err;
             }).finally()
+
+        return participant;
  }
 
